2e573af25149534587bd5a111e4e1a948da70bf7
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-02-03 00:14:55 +0530

    New

---
+++ Slicing.py
@@ -0,0 +1,18 @@
+#str1="Shivam"
+#print(str1[0:5])
+#print(str1[0:6])
+#print(str1[2:4])
+
+
+# name="ShivamShekhar"
+# name1=name[0:6]
+# name2=name[6:14]
+# print(name1,name2)
+
+
+str1="Shivam"
+print(str1[:])
+print(str1[:6])
+print(str1[:10])
+print(str1[0:])
+print(str1[0:len(str1)])
