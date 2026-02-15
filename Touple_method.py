d7d1bca589e14ebdc5b5d46c2d6285b45b13d7c0
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-02-15 17:12:34 +0530

    New

---
+++ Touple_method.py
@@ -0,0 +1,13 @@
+marks=(45,48,76,48,92,78,78,95)
+print(marks.index(48))
+print(marks.index(76))
+print(marks.index(78))
+print(marks.index(78))
+
+
+
+marks=(48,48,76,48,92,78,78,95)
+print(marks.count(48))
+print(marks.count(76))
+print(marks.count(78))
+print(marks.count(78))
